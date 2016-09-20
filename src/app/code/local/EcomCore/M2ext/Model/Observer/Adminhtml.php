1ecb717f49fa30d66c64055e2eca13987b08e6a0
Colin Macdonald <colin@indemand.com.au>
2016-09-20 14:30:56 +1000

    Final cleanup

--- src/app/code/local/EcomCore/M2ext/Model/Observer/Adminhtml.php
+++ src/app/code/local/EcomCore/M2ext/Model/Observer/Adminhtml.php
@@ -3,18 +3,45 @@
 class EcomCore_M2ext_Model_Observer_Adminhtml
 {
 
-    public function onBlockHtmlBefore(Varien_Event_Observer $observer) {
+    public function onBlockHtmlBefore(Varien_Event_Observer $observer)
+    {
         $block = $observer->getBlock();
         if (!isset($block)) return;
 
         switch ($block->getType()) {
             case 'adminhtml/catalog_product_grid':
-                $block->addColumn('ebayitemid', array(
-                    'header' => Mage::helper('eccm2ext')->__('eBay Item ID'),
-                    'index'  => 'ebayitemid',
-                ));
+                $block->addColumnAfter('ebayitemid', array(
+                    'header'   => Mage::helper('eccm2ext')->__('eBay Item ID'),
+                    'index'    => 'ebayitemid',
+                    'width'    => '100px',
+                    'sortable' => false,
+                    'filter_index' => 'm2eitem.item_id',
+                    'frame_callback' => array($this, 'callbackColumnEbayItemId'),
+                    'filter_condition_callback' => array($this, 'itemFilter'),
+                ), 'sku');
+
+                $block->sortColumnsByOrder();
+
                 break;
         }
+    }
+
+    public function callbackColumnEbayItemId($value, $row, $column, $isExport)
+    {
+        if (is_null($value) || $value === '') {
+            return '';
+        }
+
+        return '<a href="http://ebay.com.au/itm/-/' . $value . '" target="_blank">'.$value.'</a>';
+    }
+
+    public function itemFilter($collection, $column)
+    {
+
+        $field = $column->getFilterIndex() ? $column->getFilterIndex() : $column->getIndex();
+        $value = $column->getFilter()->getValue();
+        $collection->getSelect()->where("$field=?", $value); // just as an example
+
     }
 
 }
--- src/app/code/local/EcomCore/M2ext/Model/Observer/Collection.php
+++ src/app/code/local/EcomCore/M2ext/Model/Observer/Collection.php
@@ -3,53 +3,8 @@
 class EcomCore_M2ext_Model_Observer_Collection
 {
 
-<<<<<<< HEAD:src/app/code/local/EcomCore/M2ext/Model/Observer/Collection.php
-    public function onEavLoadBefore(Varien_Event_Observer $observer) {
-=======
-    public function onBlockHtmlBefore(Varien_Event_Observer $observer)
-    {
-        $block = $observer->getBlock();
-        if (!isset($block)) return;
-
-        switch ($block->getType()) {
-            case 'adminhtml/catalog_product_grid':
-                $block->addColumnAfter('ebayitemid', array(
-                    'header'   => Mage::helper('eccm2ext')->__('eBay Item ID'),
-                    'index'    => 'ebayitemid',
-                    'width'    => '100px',
-                    'sortable' => false,
-                    'filter_index' => 'm2eitem.item_id',
-                    'frame_callback' => array($this, 'callbackColumnEbayItemId'),
-                    'filter_condition_callback' => array($this, 'itemFilter'),
-                ), 'sku');
-
-                $block->sortColumnsByOrder();
-
-                break;
-        }
-    }
-
-    public function callbackColumnEbayItemId($value, $row, $column, $isExport)
-    {
-        if (is_null($value) || $value === '') {
-            return '';
-        }
-
-        return '<a href="http://ebay.com.au/itm/-/' . $value . '" target="_blank">'.$value.'</a>';
-    }
-
-    public function itemFilter($collection, $column)
-    {
-
-        $field = $column->getFilterIndex() ? $column->getFilterIndex() : $column->getIndex();
-        $value = $column->getFilter()->getValue();
-        $collection->getSelect()->where("$field=?", $value); // just as an example
-
-    }
-
     public function onEavLoadBefore(Varien_Event_Observer $observer)
     {
->>>>>>> 461197b0b9a9bfab437b05e45505f0df594bb53b:src/app/code/local/EcomCore/M2ext/Model/Adminhtml/Observer.php
         $collection = $observer->getCollection();
         if (!isset($collection)) return;
 
